51dd0d522a724259c3084b547f81bd78cd56edf8
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2018-02-02 17:56:04 -0500

    funky color-changing checkerboard

--- src/dojo/02-checkerboard.js
+++ src/dojo/02-checkerboard.js
@@ -67,15 +67,41 @@ class CheckerBoard extends React.Component {
   }
 
   decrement = () => {
-    if (this.state.size > 1)
+    if (this.state.size > 3)
       this.setState({size: this.state.size - 2})
   }
+
+  ColorChanger = props => {
+    let {color} = props
+    return React.createElement('select', {
+      value: this.state[color],
+      onChange: this[color + 'Update'],
+      style: {width: 60}
+    },
+      React.createElement('option', {value: 'black'}, 'Black'),
+      React.createElement('option', {value: 'red'}, 'Red'),
+      React.createElement('option', {value: 'blue'}, 'Blue'),
+      React.createElement('option', {value: 'green'}, 'Green'),
+      React.createElement('option', {value: 'orange'}, 'Orange'),
+      React.createElement('option', {value: 'yellow'}, 'Yellow'),
+      React.createElement('option', {value: 'pink'}, 'Pink'),
+      React.createElement('option', {value: 'purple'}, 'Purple'),
+      React.createElement('option', {value: 'brown'}, 'Brown'),
+    )
+  }
+
+  colorAUpdate = event => this.setState({colorA: event.target.value})
+  colorBUpdate = event => this.setState({colorB: event.target.value})
   
   Control = props => {
     return React.createElement('div', null, 
-      React.createElement('p', null, 'Resize (max 20): ',
+      React.createElement('p', null, 'Resize (2-20): ',
         React.createElement('button', {onClick: this.decrement}, '-'),
         React.createElement('button', {onClick: this.increment}, '+')
+      ),
+      React.createElement('p', null, 'Update colors: ',
+        React.createElement(this.ColorChanger, {color: 'colorA'}),
+        React.createElement(this.ColorChanger, {color: 'colorB'}),
       )
     )
   }
